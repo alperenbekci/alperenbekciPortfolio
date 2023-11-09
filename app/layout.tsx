093ab413cade4fbf41c1fbb3730dbdc5079b1e66
alperen bekçi <104009709+alperenbekci@users.noreply.github.com>
2023-11-09 16:38:36 +0300

    Update layout.tsx
    colors deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-neutral-900 relative pt-28 sm:pt-36 dark:bg-neutral-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#9d6ca7] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#9d6ca7]"></div>
-        <div className="bg-[#f1a2a2] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#885c5c]"></div>
+        
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
